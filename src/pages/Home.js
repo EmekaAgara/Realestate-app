3bdf5cc551e9e6ba13e3a017ff8fc9f04461fe56
Emeka Agara <66171600+EmekaAgara@users.noreply.github.com>
2023-02-28 12:16:28 +0100

    logo added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,17 @@
 import React from "react";
 import "./Home.css";
 import { Link } from "react-router-dom";
+import bg from "../images/homeBG.jpg"
+import logo from "../images/rentr.png"
 
 const Home = () => {
 
   return (
     <>
-      <Link to="/rentals"> Rentals </Link>
+      <div className="container" style={{backgroundImage:`url(${bg})`}}>
+        <div className="containerGradient"></div>
+
+      </div>
     </>
   );
 };
